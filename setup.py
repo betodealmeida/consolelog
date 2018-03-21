13080f53eec725f8c31379e6c35fcb17b08a3e9e
Beto Dealmeida <roberto@dealmeida.net>
2018-03-21 16:15:23 -0700

    New setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,134 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
 
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'console_log'
+DESCRIPTION = 'Log to browser console'
+URL = 'https://github.com/betodealmeida/consolelog'
+EMAIL = 'beto@lyft.com'
+AUTHOR = 'Beto Dealmeida'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'werkzeug',
+]
+
+development_extras = [
+    'nose',
+    'pipreqs',
+    'twine',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open('README.rst') as f:
-    readme = f.read()
+    long_description = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+exec(f.read(), about)
+
+
+class UploadCommand(Command):
+"""Support setup.py upload."""
+
+description = 'Build and publish the package.'
+user_options = []
+
+@staticmethod
+def status(s):
+    """Prints things in bold."""
+    print('\033[1m{0}\033[0m'.format(s))
+
+def initialize_options(self):
+    pass
+
+def finalize_options(self):
+    pass
+
+def run(self):
+    try:
+        self.status('Removing previous builds…')
+        rmtree(os.path.join(here, 'dist'))
+    except OSError:
+        pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='console_log',
-    version='0.1.0',
-    description='Log to browser console',
-    long_description=readme,
-    author='Beto Dealmeida',
-    author_email='beto@lyft.com',
-    url='https://github.com/betodealmeida/consolelog',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    entry_points={
+        'console_scripts': [
+            'druiddb = druiddb.console:main',
+        ],
+        'sqlalchemy.dialects': [
+            'druid = druiddb.druid_sqlalchemy:DruidHTTPDialect',
+            'druid.http = druiddb.druid_sqlalchemy:DruidHTTPDialect',
+            'druid.https = druiddb.druid_sqlalchemy:DruidHTTPSDialect',
+        ],
+    },
+    install_requires=REQUIRED,
+    extras_require={
+        'dev': development_extras,
+        'sqlalchemy': sqlalchemy_extras,
+        'cli': cli_extras,
+    },
+    include_package_data=True,
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['werkzeug'],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
